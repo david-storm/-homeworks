590f66e6294714c2ea86eb80da0824b198f4eea4
david-storm <venia7vedmak@gmail.com>
2019-10-16 17:39:18 +0300

    add ps3 : my select

---
+++ PS_3/Select/script.js
@@ -0,0 +1,34 @@
+const SOCIAL_NETWORKS = [
+    {'name': 'Facebook',
+    'class image': 'facebook'},
+    {'name':'Instagram',
+    'class image': 'inst'},
+    {'name':'YouTube',
+    'class image': 'youtube'}
+    
+];
+$(document).ready(function () {
+    
+    $(".select").html("<div class='def'>Select social networks</div>");
+
+    $.each(SOCIAL_NETWORKS, function(i, obj) {
+        $(".def").after(`<div class="option"><span class="icon ${obj['class image']}"></span>
+        <p>${obj.name}</p></div>`);
+    });
+    
+    $(".option").hide();
+    $(".def").addClass("option");
+
+    $(".option").click(function(){
+        $(this).toggleClass("option");
+        $(".option").slideToggle();
+        $(this).toggleClass("option");
+    });
+
+    $(".option").hover(function() {
+        $( this ).addClass('hover');
+    }, function() {
+        $( this ).removeClass('hover');
+    });
+
+});
