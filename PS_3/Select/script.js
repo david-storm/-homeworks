f52b4dc320b6023ada9ee960a63d7f0e383dab5c
david-storm <venia7vedmak@gmail.com>
2019-11-06 00:42:54 +0200

    fix select

--- PS_3/Select/script.js
+++ PS_3/Select/script.js
@@ -4,31 +4,57 @@ const SOCIAL_NETWORKS = [
     {'name': 'Instagram',
         'class image': 'inst' },
     {'name': 'YouTube',
-        'class image': 'youtube'}
+        'class image': 'youtube'},
+    {'name': 'Telergam',
+        'class image': 'telegram'}
 ];
 $(document).ready(function () {
 
-    $(".select").html("<div class='def arrow-down select-item selected'>Select social networks</div>");
-
+    //create select
+    $(".select").html('<div class="end-border arrow-down selected"></div>');
+    $(".selected").append('<div class="def select-item default">Select social networks</div>');
+    $(".selected").after('<div class="option "></div>');
     $.each(SOCIAL_NETWORKS, function (i, obj) {
-        $(".def").after(`<div class="option select-item"><span class="icon ${obj['class image']}"></span>
+        $(".option").append(`<div class="select-item"><span class="icon ${obj['class image']}"></span>
         <p>${obj.name}</p></div>`);
     });
 
     $(".option").hide();
+    let optionHide = true;
 
-    $(".select-item").on("click", function () {
-        if ($(this).hasClass('selected')) {
-            $(this).toggleClass("arrow-down arrow-up");
+    //expand the list of options
+    const expandOption = () => {
+        $(".option").slideToggle('fast');
+        $(".selected").toggleClass("end-border");
+        $(".selected").toggleClass("arrow-down arrow-up");
+        optionHide = false;
+    };
+    //collapse options list
+    const collapseOption = () => {
+        $(".option").slideToggle('fast');
+        setTimeout( () => {
+            $(".selected").toggleClass("end-border")
+        }, 200); 
+        $(".selected").toggleClass("arrow-down arrow-up");
+        optionHide=true;
+    };
+
+    $(document).on("click", ".selected", function () {
+        $(".option").stop(true);
+        if(optionHide){
+            expandOption();
+        } else {
+            collapseOption();
         }
-        if ($(this).hasClass('option')) {
-            $(".selected").addClass("option");
-            $(".selected").removeClass("arrow-up");
-            $(".selected").removeClass("selected");
-            $(this).addClass("selected");
-            $(this).addClass("arrow-down");
-            $(this).removeClass("option");
-        }
-        $(".option").slideToggle();
+    });
+
+    //handle click with the choice of a new option
+    $(document).on("click", ".option div", function () {
+        $(".option").stop();
+        $(".selected div").remove();
+        $(".option div").show();
+        $(this).clone().prependTo($(".selected"));
+        $('.option').find($(this)).hide();
+        collapseOption();
     });
 });
