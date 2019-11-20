0f59e6fce405b257f5a7c8b6b1df73c6db3b7c3d
david-storm <venia7vedmak@gmail.com>
2019-11-20 17:48:14 +0200

    add files

---
+++ PS_5/chat/index.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+//$doc_root = $_SERVER['DOCUMENT_ROOT'];
+//$project_puth = '/PS_5/chat/';
+//$filename =$doc_root . $project_puth . 'login.html';
+//readfile($filename);
+
+
+include 'login.html';
+//require 'login.html';
+
+//include 'chat.html';
